3f607db0f3ebd8bb92854d109e6289bfceed80a6
TJ O'Connor <toconnor@hubspot.com>
2017-09-06 02:06:14 -0400

    Travis AppEngine deploy

--- install.sh
+++ install.sh
@@ -1,2 +1,7 @@
 #/usr/bin/env sh
-echo "Installing MistyCMS..."
+echo "Deploying to QA"
+appcfg.py --module=qa --version=$BUILD_NUMBER --oauth2_access_token=$GAE_ACCESS --oauth2_refresh_token=$GAE_REFRESH update src/app.yaml
+appcfg.py --module=qa --version=$BUILD_NUMBER --oauth2_access_token=$GAE_ACCESS --oauth2_refresh_token=$GAE_REFRESH set_default_version src/app.yaml
+
+echo "Deploying to Production"
+appcfg.py --version=$BUILD_NUMBER --oauth2_access_token=$GAE_ACCESS --oauth2_refresh_token=$GAE_REFRESH update src/app.yaml
